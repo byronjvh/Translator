aefcdc0c31f5488fa8971a8c67abe4fa16936420
byronjvh <byronj.v.h.123@gmail.com>
2023-06-15 21:29:46 -0600

    Fix CSS

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -7,6 +7,7 @@ import Languages from '../Languages.json'
 import 'react-toastify/dist/ReactToastify.css'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
+import { toast } from 'react-toastify'
 
 export const globalContext = createContext()
 
