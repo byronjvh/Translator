d5521ab35ac5839856b3a356a8386ed21c8d9f1c
byronjvh <byronj.v.h.123@gmail.com>
2023-06-15 21:56:51 -0600

    Fix Height

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { ToastContainer } from 'react-toastify'
 import Translator from './context/GlobalContext'
+import { useEffect, useState } from 'react'
 
 function App () {
+  const [height, setHeight] = useState('')
+
+  useEffect(() => {
+    const viewHeight = window.innerHeight
+    setHeight(viewHeight + 'px')
+  })
+
   return (
     <>
-      <main className="app">
+      <main style={{ height }} className="app">
         <Translator />
       </main>
       <ToastContainer />
